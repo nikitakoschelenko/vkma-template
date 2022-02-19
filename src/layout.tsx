81c0b278443ef214cfdf03999087b0d4c765a41c
ItzNeviKat <nikitakoschelenko@gmail.com>
2022-02-19 16:24:49 +0300

    chore(deps)

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,6 +1,7 @@
-import { type FC } from 'react';
-import { ModalRoot, PopoutRoot, View } from '@itznevikat/router';
+import { type ReactElement, type FC } from 'react';
+import { ModalRoot, useParams, View } from '@itznevikat/router';
 import { ScreenSpinner } from '@vkontakte/vkui';
+import { getNavId } from '@vkontakte/vkui/dist/lib/getNavId';
 import { Icon28HomeOutline, Icon28InfoOutline } from '@vkontakte/icons';
 
 import { AdaptivityLayout } from '@/adaptivity';
@@ -15,43 +16,51 @@ import {
 } from '@/home';
 import { Info } from '@/info';
 
-export const Layout: FC = () => (
-  <AdaptivityLayout
-    modal={
-      <ModalRoot>
-        <TestModalCard nav="test-modal-card" />
-      </ModalRoot>
-    }
-    popout={
-      <PopoutRoot>
-        <ScreenSpinner id="screen-spinner" />
-        <TestActionSheet nav="test-action-sheet" />
-        <TestAlert nav="test-alert" />
-      </PopoutRoot>
-    }
-    buttons={[
-      {
-        icon: <Icon28HomeOutline />,
-        story: '/',
-        text: 'Главная'
-      },
-      {
-        icon: <Icon28InfoOutline />,
-        story: '/info',
-        text: 'О приложении'
+function matchPopout(popout: string | null, elements: ReactElement[]) {
+  return popout
+    ? elements.find(({ props }) => getNavId(props) === popout)
+    : null;
+}
+
+export const Layout: FC = () => {
+  const { popout = null } = useParams();
+
+  return (
+    <AdaptivityLayout
+      modal={
+        <ModalRoot>
+          <TestModalCard nav="test-modal-card" />
+        </ModalRoot>
       }
-    ]}
-  >
-    <View nav="/">
-      <Home nav="/" />
-      <Persik nav="/persik" />
-      <Components nav="/components" />
+      popout={matchPopout(popout, [
+        <ScreenSpinner id="screen-spinner" />,
+        <TestActionSheet nav="test-action-sheet" />,
+        <TestAlert nav="test-alert" />
+      ])}
+      buttons={[
+        {
+          icon: <Icon28HomeOutline />,
+          story: '/',
+          text: 'Главная'
+        },
+        {
+          icon: <Icon28InfoOutline />,
+          story: '/info',
+          text: 'О приложении'
+        }
+      ]}
+    >
+      <View nav="/">
+        <Home nav="/" />
+        <Persik nav="/persik" />
+        <Components nav="/components" />
 
-      <Fallback nav="/404" />
-    </View>
+        <Fallback nav="/404" />
+      </View>
 
-    <View nav="/info">
-      <Info nav="/" />
-    </View>
-  </AdaptivityLayout>
-);
+      <View nav="/info">
+        <Info nav="/" />
+      </View>
+    </AdaptivityLayout>
+  );
+};
